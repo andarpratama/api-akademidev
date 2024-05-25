2ebee9c2ca943d5ab473be9bb478bcb49ae3135b
andarpratama <andar.webdev@gmail.com>
2024-05-25 23:48:32 +0700

    mysql connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,42 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import * as dotenv from 'dotenv';
+import { Connection } from 'typeorm';
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly connection: Connection) {
+    this.checkDatabaseConnection();
+  }
+  private readonly logger = new Logger(AppModule.name);
+
+
+  private async checkDatabaseConnection() {
+    try {
+      await this.connection.query('SELECT 1');
+      this.logger.log('Database connection is successful.');
+    } catch (error) {
+      console.error('Failed to connect to the database:', error);
+    }
+  }
+}
